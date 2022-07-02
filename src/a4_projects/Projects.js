bac60d3593d1104f9c1c702f68c23ea7e4b6ae91
raitasu <pavelburyak3@gmail.com>
2022-07-03 00:17:54 +0300

    feat: changed projects view

--- src/a4_projects/Projects.js
+++ src/a4_projects/Projects.js
@@ -3,12 +3,11 @@ import styles from './Projects.module.scss';
 import {Project} from "./project/Project";
 import Title from "../common/components/title/Title";
 import imgOne from './../assets/images/gallery-photo-1.png'
-import imgTwo from '../assets/images/gallery-photo-2.png'
 import imgThree from '../assets/images/gallery-photo-3.png'
-import imgFour from '../assets/images/gallery-photo-4.png'
 import imgFive from '../assets/images/gallery-photo-5.png'
 import imgSix from '../assets/images/gallery-photo-6.png'
 import imgEight from '../assets/images/gallery-photo-8.png'
+import imgFour from '../assets/images/gallery-photo-4.png'
 
 
 function Projects() {
@@ -22,17 +21,29 @@ function Projects() {
                         img={imgOne}
                         description={"CSS, JavaScript, TypeScript, React, Redux, Material UI,Using TypeScript for best code control,Bug fixing, deploying,Covering code with unit tests."}
                         link={"https://raitasu.github.io/todolist"}/>
-                    {/*<Project*/}
-                    {/*    title={"Counter"}*/}
-                    {/*    img={imgTwo}*/}
-                    {/*    description={"My first project with React/Redux/TS.I left It here as a memory."}*/}
-                    {/*    link={"https://raitasu.github.io/social-network"}/>*/}
+                    <Project
+                        title={"future project"}
+                        img={imgFour}
+                        description={"...in progress"}
+                        link={"link #1"}/>
 
                     <Project
                         title={"The social network"}
                         img={imgThree}
                         description={"SCSS,JavaScript,TypeScript,React,Redux,Redux-thunk,Formik,Axios,Reselect,TypeScript,Refactoring,bug fixing,deploying,Organizing correct app architecture; Covering code with unit tests. "}
                         link={"https://raitasu.github.io/social-network/"}/>
+                    <Project
+                        title={"Midas pizza"}
+                        img={imgFive}
+                        description={"ReactJS,TypeScript,Redux Toolkit, React Router v6, Axios + Fetch,React Hooks,Prettier,CSS-Modules/SCSS,React Content Loader,React Pagination,Lodash.Debounce,Code Splitting, React Loadable, useWhyDidYouUpdate"}
+                        link={"https://raitasu.github.io/midas-pizza/"}/>
+                    {/*<Project*/}
+                    {/*    title={"Counter"}*/}
+                    {/*    img={imgTwo}*/}
+                    {/*    description={"My first project with React/Redux/TS.I left It here as a memory."}*/}
+                    {/*    link={"https://raitasu.github.io/social-network"}/>*/}
+
+
 
                     {/*<Project*/}
                     {/*    title={"Study Point"}*/}
@@ -40,11 +51,7 @@ function Projects() {
                     {/*    description={"It was a command work. App for learning some cards"}*/}
                     {/*    link={"https://raitasu.github.io/todolist"}/>*/}
 
-                    <Project
-                        title={"Midas pizza"}
-                        img={imgFive}
-                        description={"ReactJS,TypeScript,Redux Toolkit, React Router v6, Axios + Fetch,React Hooks,Prettier,CSS-Modules/SCSS,React Content Loader,React Pagination,Lodash.Debounce,Code Splitting, React Loadable, useWhyDidYouUpdate"}
-                        link={"https://raitasu.github.io/midas-pizza/"}/>
+
 
                     <Project
                         title={"future project"}
@@ -52,17 +59,14 @@ function Projects() {
                         description={"...in progress"}
                         link={"link #6"}/>
 
-                    <Project
-                        title={"future project"}
-                        img={'https://thumbs.dreamstime.com/b/clock-11436808.jpg'}
-                        description={"...in progress"}
-                        link={"link #1"}/>
+
 
                     <Project
                         title={"future project "}
                         img={imgEight}
                         description={"...in progress"}
                         link={"link #2"}/>
+
                 </div>
             </div>
         </div>
